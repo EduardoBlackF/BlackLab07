c0affac8c89cf644cc48d274f578871248bbc7a3
EduardoBlackF <eduardo.black@tecsup.edu.pe>
2023-05-20 00:00:52 -0500

    Agregar archivos de proyecto.

---
+++ Demo072023C/Demo072023C/BatteryDemo.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Demo072023C.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo072023C
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BatteryDemo : ContentPage
+    {
+        public BatteryDemo()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+
+            Button backButton = new Button
+            {
+                Text = "Back",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            backButton.Clicked += BackButton_Clicked;
+
+            var button = new Button
+            {
+                Text = "Click for battery info",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+
+            button.Clicked += (sender, e) =>
+            {
+                var bat = DependencyService.Get<IBattery>();
+
+                switch (bat.PowerSource)
+                {
+                    case PowerSource.Battery:
+                        button.Text = "Battery - ";
+                        break;
+                    case PowerSource.Ac:
+                        button.Text = "AC - ";
+                        break;
+                    case PowerSource.Usb:
+                        button.Text = "USB - ";
+                        break;
+                    case PowerSource.Wireless:
+                        button.Text = "Wireless - ";
+                        break;
+                    case PowerSource.Other:
+                    default:
+                        button.Text = "Unknown - ";
+                        break;
+                }
+                switch (bat.Status)
+                {
+                    case BatteryStatus.Charging:
+                        button.Text += "Charging";
+                        break;
+                    case BatteryStatus.Discharging:
+                        button.Text += "Discharging";
+                        break;
+                    case BatteryStatus.NotCharging:
+                        button.Text += "Not Charging";
+                        break;
+                    case BatteryStatus.Full:
+                        button.Text += "Full";
+                        break;
+                    case BatteryStatus.Unknown:
+                    default:
+                        button.Text += "Unknown";
+                        break;
+                }
+            };
+
+
+            stack.Children.Add(button);
+            stack.Children.Add(backButton);
+            Content = stack;
+
+        }
+
+        private void BackButton_Clicked(object sender, EventArgs e)
+        {
+
+            Navigation.PopAsync();
+        }
+    }
+}
+
+
